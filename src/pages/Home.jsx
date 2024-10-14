9fce9bce67ff24d7382818c6616b6ce856e8e3bd
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-14 18:00:33 +0500

    UI changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import Banner from "../components/Banner"
 import NavBar from '../components/NavBar'
 import Home2 from '../components/Home2'
+import mountainImage from "../assets/mountainImage.jpg";
+
 export default function Home() {
     return (
         <>
-            <Banner />
-            <NavBar />
-            <Home2 />
+            <div
+                className='relative h-screen bg-cover bg-center'
+                style={{ backgroundImage: `url(${mountainImage})` }}
+            >
+                <div className='absolute inset-0 bg-gradient-to-b from-white/30 to-white/0'></div>
+                <Banner />
+                <NavBar />
+                <Home2 />
+            </div>
         </>
     )
 }
