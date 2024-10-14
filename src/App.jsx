eb3901b27b761845753e9355b81b24c2a157746b
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-14 11:50:29 +0500

    basic code setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+function App() {
+
+  return (
+    <>
+      <h1 className='grid place-items-center text-blue-700 lg:text-black'>Hello from React.js</h1>
+    </>
+  )
+}
+
+export default App
