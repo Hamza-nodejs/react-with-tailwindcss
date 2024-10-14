bd761dbf0eb0e40a6b325a1228e2069b657ea62e
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-14 15:37:33 +0500

    diclosure.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
 import Card from './components/Card'
+import DisclosureComponent from './components/DisclosureComponent'
 function App() {
 
   return (
     <>
-      <Card name="Nike" price="500" status="in-stock" />
+      <div>
+        <Card name="Nike" price="500" status="in-stock" />
+        <container className="question flex flex-row">
+        <DisclosureComponent />
+        <DisclosureComponent />
+        </container>
+
+      </div>
     </>
   )
 }
