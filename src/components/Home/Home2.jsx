47beecac83bb82d679da04dbb1a75f2c94afaff8
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-15 17:37:15 +0500

    responsive

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -6,18 +6,20 @@ export default function Home2() {
         <>
             <div className="home flex">
                 {/* Left Section */}
-                <div className=' w-1/2 flex items-center flex-col p-4 pt-24'>
-                    <p className=' mb-2 text-5xl text-white'>Welcome to Eqlize Now.</p>
-                    <h3 className='font-mono mb-8 text-2xl text-white'>
+                <div className=' lg:w-1/2 lg:flex lg:items-center lg:flex-col lg:p-4 lg:pt-24'>
+                    <p className='mx-4 text-xl text-black font-bold lg:mb-2 lg:text-5xl lg:text-white'>Welcome to Eqlize Now.</p>
+                    <h3 className='font-semibold text-white mx-4 pt-2 lg:font-mono lg:mb-8 lg:text-sm lg:text-white'>
                         Crowdfund your future
                     </h3>
-                    <p className='text-lg text-white
+                    <p className='text-sm text-white mx-4 pt-5 lg:text-lg lg:text-white
                     '>
                         Fund your new home, college, start-up, or anything important
                     </p>
-                    <button className='cursor-copy p-2 rounded-lg bg-blue-500 text-white my-3 font-bold'>Get started</button>
-                    <p className='text-white text-sm font-extralight'>Back a campaign to launch yours for free.</p>
-                    <button className='bg-black text-white p-2 mt-4 border border-white rounded-2xl'>Got an Invite?</button>
+                    <div className=''>
+                        <button className='bg-black text-white text-xs  rounded-md mx-5 mt-3 p-2  lg:cursor-copy lg:p-1  lg:rounded-lg lg:bg-blue-500 lg:text-white lg:my-3 lg:font-bold'>Get started</button>
+                    </div>
+                    <p className='text-base mx-4 text-white mt-2 lg:text-white lg:text-sm lg:font-extralight'>Back a campaign to launch yours for free.</p>
+                    <button className='bg-black text-white p-2 mt-2 mx-9  border-white rounded-xl font-thin lg:p-2 lg:mt-4 lg:border lg:rounded-2xl'>Got an Invite?</button>
                 </div>
 
                 {/* Right Section */}
