e0cfa3417459c8ff1142b273a11caaf30f742a1d
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-15 13:04:11 +0500

    changes

--- src/components/Home/Home2.jsx
+++ src/components/Home/Home2.jsx
@@ -6,18 +6,18 @@ export default function Home2() {
         <>
             <div className="home flex">
                 {/* Left Section */}
-                <div className=' w-1/2 flex items-center flex-col p-6 pt-24'>
-                    <p className='mb-6 text-5xl text-slate-400'>Welcome to Eqlize Now.</p>
-                    <h3 className='font-extrabold mb-8 text-3xl'>
+                <div className=' w-1/2 flex items-center flex-col p-4 pt-24'>
+                    <p className=' mb-2 text-5xl text-white'>Welcome to Eqlize Now.</p>
+                    <h3 className='font-mono mb-8 text-2xl text-white'>
                         Crowdfund your future
                     </h3>
-                    <p className='text-xl
+                    <p className='text-lg text-white
                     '>
                         Fund your new home, college, start-up, or anything important
                     </p>
-                    <button className='p-4 rounded-lg bg-blue-500 text-white my-4'>Get started</button>
-                    <p>Back a campaign to launch yours for free.</p>
-                    <button className='bg-black text-white cursor-pointer p-2 mt-4'>Got an Invite?</button>
+                    <button className='cursor-copy p-2 rounded-lg bg-blue-500 text-white my-3 font-bold'>Get started</button>
+                    <p className='text-white text-sm font-extralight'>Back a campaign to launch yours for free.</p>
+                    <button className='bg-black text-white p-2 mt-4 border border-white rounded-2xl'>Got an Invite?</button>
                 </div>
 
                 {/* Right Section */}
